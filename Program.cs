aabc7d8fea8d5e147dbcc428328f8393336d7d41
Alexandr <jwchaganav@gmail.com>
2022-11-25 14:08:25 +0400

    последний элемент не считает

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int numBegin = InputInt("Введите начальное значение ");
+int numEnd = InputInt("Введите конечное значение ");
+
+int sumNumbers = 0;
+sumNumbers = SumNumber(numBegin, numEnd);
+
+Console.WriteLine($"{sumNumbers}");
+
+int SumNumber(int min, int max)
+{
+    int sum;
+    if (max==min)
+        return min;
+    else
+    {
+        max--;
+        Console.WriteLine($"{max}");
+        sum = max + SumNumber(min, max);
+        Console.WriteLine($"sum = {sum}");
+        return sum;
+    }   
+}
+
+
+int InputInt(string message)
+{
+    Console.WriteLine(message);
+    Console.Write("> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
